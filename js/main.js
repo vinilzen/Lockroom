e31e447a3eabc153033f5a319ad830e5079141c2
vinilzen <marchukilya@gmail.com>
2014-06-10 16:04:48 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('.time').click(function(){
+		var str =	$(this).attr('data-date') +
+					' ('+ $(this).attr('data-day') +') '+
+					$(this).attr('data-time') +
+					'<br><strong>'+$(this).attr('data-price')+' </strong>руб.';
+		$('.modal-title').html(str);
+		$('.formaModal').modal('show');
+	});
+});
